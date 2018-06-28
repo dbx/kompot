05f166a37ceb8daedbd8aaaceca5ef661382c1e3
Janos Erdos <janos.erdos@dbx.hu>
2018-06-28 13:41:27 +0200

    wip

--- src/hu/dbx/kompot/consumer/async/handler/SelfDescribingEventProcessor.java
+++ src/hu/dbx/kompot/consumer/async/handler/SelfDescribingEventProcessor.java
@@ -4,6 +4,7 @@ import hu.dbx.kompot.consumer.async.EventDescriptor;
 import hu.dbx.kompot.consumer.async.EventStatusCallback;
 
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public interface SelfDescribingEventProcessor<TReq> {
 
@@ -24,6 +25,26 @@ public interface SelfDescribingEventProcessor<TReq> {
             @Override
             public void handle(TReq request, EventStatusCallback callback) {
                 bc.accept(request, callback);
+            }
+        };
+    }
+
+    static <TReq> SelfDescribingEventProcessor<TReq> of(EventDescriptor<TReq> event, Consumer<TReq> handler) {
+        return new SelfDescribingEventProcessor<TReq>() {
+
+            @Override
+            public EventDescriptor<TReq> getEventMarker() {
+                return event;
+            }
+
+            @Override
+            public void handle(TReq request, EventStatusCallback callback) {
+                try {
+                    handler.accept(request);
+                    callback.success("OK");
+                } catch (Throwable t) {
+                    callback.error(t);
+                }
             }
         };
     }
