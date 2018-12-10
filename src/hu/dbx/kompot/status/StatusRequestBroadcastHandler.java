e899e54eb552f22533cb5b6a97d59b4a1f1ec8a1
janos erdos <erdosj@gmail.com>
2018-12-10 19:56:21 +0100

    logging

--- src/hu/dbx/kompot/status/StatusRequestBroadcastHandler.java
+++ src/hu/dbx/kompot/status/StatusRequestBroadcastHandler.java
@@ -4,7 +4,9 @@ import hu.dbx.kompot.consumer.broadcast.handler.BroadcastDescriptor;
 import hu.dbx.kompot.consumer.broadcast.handler.SelfDescribingBroadcastProcessor;
 import hu.dbx.kompot.core.SerializeHelper;
 import hu.dbx.kompot.exceptions.SerializationException;
+import hu.dbx.kompot.impl.LoggerUtils;
 import hu.dbx.kompot.impl.consumer.ConsumerConfig;
+import org.slf4j.Logger;
 import redis.clients.jedis.Jedis;
 
 import java.util.Map;
@@ -17,7 +19,7 @@ import java.util.function.Supplier;
  */
 public class StatusRequestBroadcastHandler implements SelfDescribingBroadcastProcessor<Map<String, Object>> {
 
-    // TODO: logging
+    private static final Logger LOGGER = LoggerUtils.getLogger();
 
     @SuppressWarnings("unchecked")
     private static final BroadcastDescriptor<Map<String, Object>> DESCRIPTOR = BroadcastDescriptor.of("KMPT_SAY_HELLO", Map.class);
@@ -38,8 +40,10 @@ public class StatusRequestBroadcastHandler implements SelfDescribingBroadcastPro
     @Override
     public void handle(Map<String, Object> request) throws SerializationException {
         final String responseKey = (String) request.get("key");
+        LOGGER.debug("Writing status to key {}", responseKey);
 
         assert (responseKey != null);
+
 
         final StatusReport status = statusReportFactory.get();
         final String serialized = SerializeHelper.serializeObject(status);
@@ -47,5 +51,7 @@ public class StatusRequestBroadcastHandler implements SelfDescribingBroadcastPro
         try (final Jedis jedis = config.getPool().getResource()) {
             jedis.rpush(responseKey, serialized);
         }
+        
+        LOGGER.debug("Successfully written status to key {}", responseKey);
     }
 }
