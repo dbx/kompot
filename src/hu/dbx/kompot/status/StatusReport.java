cd639851c80ea27d80f5b583522d96b8f36ecbb6
Janos Erdos <janos.erdos@dbx.hu>
2018-12-10 12:02:26 +0100

    docs + new fns

--- src/hu/dbx/kompot/status/StatusReport.java
+++ src/hu/dbx/kompot/status/StatusReport.java
@@ -1,10 +1,11 @@
 package hu.dbx.kompot.status;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public interface StatusReport {
-
 
     UUID getModuleIdentifier();
 
@@ -12,7 +13,27 @@ public interface StatusReport {
 
     String getMessageGroup();
 
+    /**
+     * Timestamp of last time this module has written its status to the db.
+     */
+    Date getLastHeartbeatTime();
+
     List<StatusItem> getItems();
+
+    /**
+     * Possibly empty list of all methods this module is listening to.
+     */
+    Set<String> getRegisteredMethods();
+
+    /**
+     * Possibly empty lit of all event codes this module is listening to.
+     */
+    Set<String> getRegisteredEvents();
+
+    /**
+     * List of all broadcast codes this module is module is listetning to.
+     */
+    Set<String> getRegisteredBroadcasts();
 
     interface StatusItem {
 
